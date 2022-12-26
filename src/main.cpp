fdf25fa74f3c5afb6c7a112635a8d992c89375c4
Jinjun Liang <kumkee@users.noreply.github.com>
2022-12-26 19:43:50 +1300

    one bug fixed

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,13 @@ void setup() {
   }
 }
 
-
 void loop() {
   for (uint8_t d : digits) {
     for (uint8_t b : digits) {
       digitalWrite(b, HIGH);
     }
     digitalWrite(d, LOW);
-    run_thr_segments();
+    run_thr_digits();
   }
 }
 
@@ -66,75 +65,75 @@ void display_bin(uint8_t bin) {
 uint8_t char_map(char c) {
   switch (c) {
   case '0':
-    return 0b11111100; // 0
+    return 0b1111110; // 0
   case '1':
-    return 0b01100000; // 1
+    return 0b0110000; // 1
   case '2':
-    return 0b11011010; // 2
+    return 0b1101101; // 2
   case '3':
-    return 0b11110010; // 3
+    return 0b1111001; // 3
   case '4':
-    return 0b01100110; // 4
+    return 0b0110011; // 4
   case '5':
-    return 0b10110110; // 5
+    return 0b1011011; // 5
   case '6':
-    return 0b10111110; // 6
+    return 0b1011111; // 6
   case '7':
-    return 0b11100000; // 7
+    return 0b1110000; // 7
   case '8':
-    return 0b11111110; // 8
+    return 0b1111111; // 8
   case '9':
-    return 0b11110110; // 9
+    return 0b1111011; // 9
   case 'a':
-    return 0b11101110; // A/10
+    return 0b1110111; // A/10
   case 'b':
-    return 0b00111110; // b/11
+    return 0b0011111; // b/11
   case 'c':
-    return 0b10011100; // C/12
+    return 0b1001110; // C/12
   case 'd':
-    return 0b01111010; // d/13
+    return 0b0111101; // d/13
   case 'e':
-    return 0b10011110; // E/14
+    return 0b1001111; // E/14
   case 'f':
-    return 0b10001110; // F/15
+    return 0b1000111; // F/15
   case 'g':
-    return 0b10111100; // G/16
+    return 0b1011110; // G/16
   case 'h':
-    return 0b01101110; // H/17
+    return 0b0110111; // H/17
   case 'i':
-    return 0b00001100; // I/18
+    return 0b0000110; // I/18
   case 'j':
-    return 0b01111000; // J/19
+    return 0b0111100; // J/19
   case 'l':
-    return 0b00011100; // L/20
+    return 0b0001110; // L/20
   case 'n':
-    return 0b00101010; // n/21
+    return 0b0010101; // n/21
   case 'o':
-    return 0b00111010; // o/22
+    return 0b0011101; // o/22
   case 'p':
-    return 0b11001110; // P/23
+    return 0b1100111; // P/23
   case 'q':
-    return 0b11100110; // q/24
+    return 0b1110011; // q/24
   case 'r':
-    return 0b00001010; // r/25
+    return 0b0000101; // r/25
   case 's':
-    return 0b10110110; // S/26   looks like number 5
+    return 0b1011011; // S/26   looks like number 5
   case 't':
-    return 0b00011110; // t/27
+    return 0b0001111; // t/27
   case 'u':
-    return 0b01111100; // U/28
+    return 0b0111110; // U/28
   case 'y':
-    return 0b01110110; // y/29
+    return 0b0111011; // y/29
   case '-':
-    return 0b00000010; //-/30
+    return 0b0000001; //-/30
   case '.':
-    return 0b00000001; //./31
+    return 0b10000000; //./31
   case ']':
-    return 0b11110000; //]/32
+    return 0b1111000; //]/32
   case '[':
-    return 0b10011100; //[/33
+    return 0b1001110; //[/33
   case '_':
-    return 0b00010000; //_/34
+    return 0b0001000; //_/34
   default:
     return 0;
   }
