eaaced652ccdb429793601f6a1ce01d793277614
Jinjun Liang <kumkee@users.noreply.github.com>
2023-01-03 22:30:25 +1300

    Updated comments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "header.h"
 
-// Pin connected to latch pin (RCLK) of 74HC595
+// Pin connected to latch pin (RCLK) of the shift register
 const uint8_t LATCH_PIN = 33;
-// Pin connected to clock pin (SRCLK) of 74HC595
+// Pin connected to clock pin (SRCLK) of the shift register
 const uint8_t CLOCK_PIN = 26;
-// Pin connected to Data in (SER) of 74HC595
+// Pin connected to Data in (SER) of the shift register
 const uint8_t DATA_PIN = 14;
 const std::array<uint8_t, NUM_DIGITS> DIGIT_PINS = {10, 18, 19, 21};
 const unsigned delay_ms = 100;
@@ -13,13 +13,11 @@ const unsigned MULTIPLEXED_DELAY_US = 1024;
 DigitDisplay dd;
 
 void setup() {
-  // set pins to output so you can control the shift register
   Serial.begin(115200);
   dd = {false, // is_common_anode, false for common cathode display
         {LATCH_PIN, CLOCK_PIN, DATA_PIN},
         DIGIT_PINS,            // array of size NUM_DIGITS
-        MULTIPLEXED_DELAY_US}; // duration for diplaying each digit in
-                               // microseconds
+        MULTIPLEXED_DELAY_US}; // duration for diplaying each digit in μs
   init_digit_display(dd);
 }
 
