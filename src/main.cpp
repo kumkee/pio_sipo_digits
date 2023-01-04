e010af3acfc311558afffe90a0fc45edb32ddc10
Jinjun Liang <kumkee@users.noreply.github.com>
2023-01-04 20:12:42 +1300

    IS_COMMON_ANODE as a constant bool parameter

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,19 @@ const uint8_t CLOCK_PIN = 26;
 // Pin connected to Data in (SER) of the shift register
 const uint8_t DATA_PIN = 14;
 const std::array<uint8_t, NUM_DIGITS> DIGIT_PINS = {23, 18, 19, 21};
-const unsigned delay_ms = 100;
 const unsigned MULTIPLEXED_DELAY_US = 1024;
+const bool IS_COMMON_ANODE = false;
+
+const unsigned delay_ms = 100;
 
 DigitDisplay dd;
 
 void setup() {
   Serial.begin(115200);
-  dd = {false, // is_common_anode, false for common cathode display
+  dd = {IS_COMMON_ANODE,        // false for common cathode display
         {LATCH_PIN, CLOCK_PIN, DATA_PIN},
-        DIGIT_PINS,            // array of size NUM_DIGITS
-        MULTIPLEXED_DELAY_US}; // duration for diplaying each digit in μs
+        DIGIT_PINS,             // array of size NUM_DIGITS
+        MULTIPLEXED_DELAY_US};  // duration for diplaying each digit in μs
   init_digit_display(dd);
 }
 
