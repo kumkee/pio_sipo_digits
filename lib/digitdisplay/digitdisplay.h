cf9b7e051c2891deaef3e4bc246125dec7f9be4f
Jinjun Liang <kumkee@users.noreply.github.com>
2023-01-04 20:38:59 +1300

    added TODO

--- lib/digitdisplay/digitdisplay.h
+++ lib/digitdisplay/digitdisplay.h
@@ -21,5 +21,8 @@ uint8_t num_to_str(char *str, int num);
 uint8_t num_to_str(char *str, float num, uint8_t num_decimals = NUM_DIGITS);
 void display_digits(DigitDisplay dd);
 void display_string(DigitDisplay dd, char *str, uint8_t dec_pnts = 0);
+void display_number(DigitDisplay dd, int num);
+void display_number(DigitDisplay dd, float num);
+// TODO: implementation of the two above with memorization of num_to_str
 
 #endif
